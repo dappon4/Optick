afa1ed8a2689572663bc5d7f0f25b2ad221c08f0
NSP909 <nspriyadarshan@gmail.com>
2024-03-24 09:49:49 -0400

    updating frontend

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,24 +1,48 @@
-import Image from "next/image";
-import IceCream from '../../public/ice_cream.svg'
-import Diet from '../../public/diet.svg'
-import { Button } from "@/components/ui/button";
+// pages/A.js
+import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import base from "./svgs.css";
+import { Button } from "@/components/ui/button";
+
+const quotes = [
+  "The only way to do great work is to love what you do. - Steve Jobs",
+  "Believe you can and you're halfway there. - Theodore Roosevelt",
+  "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+  "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+  "It does not matter how slowly you go as long as you do not stop. - Confucius",
+];
+
+const getRandomQuote = () => {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  return quotes[randomIndex];
+};
 
 const A = () => {
+  const quote = getRandomQuote();
+
   return (
-    <div className="flex justify-center items-center min-h-[100vh] w-[100vw]">
-      <div className="flex">
-        <div className="h-94 m-4 flex-col flex justify-center space-y-4">
-          <Image src={IceCream} height={130} alt="" />
-          <Link className="flex justify-center mt-16" href="/tracker">
-            <Button className="bg-[#228B22]">Count Your Calories!</Button>
-          </Link>
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <div>
+        <h1 className="text-4xl font-semibold mb-8 text-center text-green-900">Hey Dahong!</h1>
+        <p className="text-lg italic mb-8 text-center text-gray-800">{quote}</p>
+      </div>
+      <div className="flex justify-center items-center w-full">
+        <div className={`${base.bgLeft} flex items-center justify-center w-1/2 h-full`}>
+          <Image src="/ice_cream.svg" height={480} width={480} alt="" />
+          <div className="ml-4">
+            <Link href="/tracker">
+              <Button className="bg-[#228B22] w-48 h-12">Add to Stomach!</Button>
+            </Link>
+          </div>
         </div>
-        <div className="h-94 m-4 flex-col flex justify-center space-y-4">
-          <Image src={Diet} height={130} alt="" />
-          <Link className="flex justify-center mt-16" href="/meal">
-            <Button className="bg-[#228B22]">Make a Meal!</Button>
-          </Link>
+        <div className={`${base.bgRight} flex items-center justify-center w-1/2 h-full`}>
+          <div className="ml-4 mr-4">
+            <Link href="/meal">
+              <Button className="bg-[#228B22] w-48 h-12">Add to Inventory!</Button>
+            </Link>
+          </div>
+          <Image src="/tea.svg" height={480} width={480} alt="" />
         </div>
       </div>
     </div>
